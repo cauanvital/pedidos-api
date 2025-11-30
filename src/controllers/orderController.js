20395e6da558aa16b69b756df9e34faeb76c6871
cauanvital <cauanvital74@gmail.com>
2025-11-29 22:11:58 -0300

    feat: setting controller

---
+++ src/controllers/orderController.js
@@ -0,0 +1,24 @@
+import { OrderModel } from '../models/OrderModel.js';
+
+export const orderController = {
+
+    // POST /order
+    createOrder: async (req, res) => {
+    },
+
+    // GET /order/list
+    listOrders: async (req, res) => {
+    },
+
+    // GET /order/:id
+    getOrderById: async (req, res) => {
+    },
+
+    // PUT /order/:id
+    updateOrder: async (req, res) => {
+    },
+
+    // DELETE /order/:id
+    deleteOrder: async (req, res) => {
+    }
+};
